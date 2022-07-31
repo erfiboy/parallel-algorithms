83cb69b904cd37e2ca0a21b7c80c788ab1d4f284
erfiboy <erfannosrati20@gmail.com>
2022-08-01 00:32:21 +0430

    final

--- merge_sort.c
+++ merge_sort.c
@@ -4,233 +4,49 @@
 #include <math.h>
 
 #include "timer.h"
-#include "pth_msort.h"
 #include <stdbool.h>
 
 int* value;
 unsigned int n;
 int* sort;
 
-
-
-void merge_sort(int* value, int start_index, int end_index, int* sorted);
-void mergeSortParallel(const int* values, unsigned int N, int* sorted);
-void merge(int* value, int start, int mid, int end, int* test_sorted);
-void binary(const int* values, int value);
-void* merge_thread_handler(void* thread_index);
-void* sort_thread_handler(void* thread_index);
-int compare_point(const int a , const int b);
-void struct_merge_sort(int* value, int start_index, int end_index, int* sorted, int* point_index, int* sorted_int_index, bool* point_A,bool* sorted_A);
-void struct_merge(int* value, int start, int mid, int end, int* test_sorted, int* point_index, int* sorted_int_index, bool* point_A,bool* sorted_A);
-
-void mergeSortParallel(const int* values, unsigned int N, int* sorted)
+int binary_search(const int* values, int start, int end, int value)
 {
-    value = (int*) values;
-    n = N;
-    sort = sorted;
-    int* value = (int*) values;
-    pthread_t* handles = (pthread_t*) malloc (4*sizeof(pthread_t));
-    for (int thread_index =0; thread_index<4; thread_index++){
-        pthread_create(&handles[thread_index], NULL, sort_thread_handler, (void*)thread_index);  
-    } 
-
-    for (int thread_index =0; thread_index<4; thread_index++){
-        pthread_join(handles[thread_index], NULL);  
-    } 
-
-    // merge two merge sorts 
-
-    for (int thread_index =0; thread_index<2; thread_index++){
-        pthread_create(&handles[thread_index], NULL, merge_thread_handler, (void*)thread_index);  
-    } 
-
-    for (int thread_index =0; thread_index<2; thread_index++){
-        pthread_join(handles[thread_index], NULL);  
-    }  
-
-    int step = pow(2,1);
-    int* point_values = (int*) malloc ( sizeof(int) * N/step );
-    int* sorted_int_values = (int*) malloc ( sizeof(int) * N/step );
-
-    int* point_index = (int*) malloc ( sizeof(int) * N/step );
-    int* sorted_int_index = (int*) malloc ( sizeof(int) * N/step );
-
-    bool* point_A = (bool*) malloc ( sizeof(bool) * N/step );
-    bool* sorted_int_A = (bool*) malloc ( sizeof(bool) * N/step );
-
-    for (int i = 0; i < N/(2*step); i++){
-        point_values[2*i] = values[i*step];
-        point_index[2*i] = i*step;
-        point_A[2*i] = true;
-
-        point_values[2*i+1] = values[(i*step)+N/2];
-        point_index[2*i+1] = (i*step)+N/2;
-        point_A[2*i+1]= false;
-
+    if ( end - start < 2 &&  values[end] != value && values[start] != value){
+        return end;
     }
-    printf("Hellllloooooo---------------\n");
-    for(int i = 0; i < N/pow(2,1); i++){
-        printf("index =%d, value=%d \n", point_index[i], point_values[i]);
+    if (value < values[start])
+    {
+        return start;
     }
-    struct_merge_sort(point_values, 0,  2*N/pow(2,1)-1, sorted_int_values, point_index, sorted_int_index, point_A, sorted_int_A);
 
-    printf("Hellllloooooo---------------\n");
-    for(int i = 0; i < N/pow(2,1); i++){
-        printf("index =%d, value=%d \n", point_index[i], point_values[i]);
+    if (value == values[start] || value < values[start]){
+        return start;
+    }
+    else if(value == values[end] || value > values[end]){
+        return end;
+    }
+    else if(value == values[(start+end)/2]){
+        return (start+end)/2;
+    }
+    else if (value < values[(start+end)/2]){
+        return binary_search(values, start, (start+end)/2, value);
+    }
+    else{
+        return binary_search(values, (start+end)/2+1, end, value);
     }
 }
 
-int compare_int(const int a , const int b) {
-    if (a <= b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
+
+
+int main(){
+    int* a = (int*) malloc(sizeof(int) * 10);
+    for(int i = 0; i < 10; i++){
+        a[i] = 2*i;
     }
-}
+    int * sort = (int*) malloc(sizeof(int) * 10);
+    int result = binary_search(a, 0, 9, 11);
 
-void* sort_thread_handler(void* thread_index){
-    int index = (int) thread_index;
-    merge_sort(value, index*n/4, (index+1)*n/4-1, sort);
-    return NULL;
-}
+    printf(" %d \n", result);
 
-void* merge_thread_handler(void* thread_index){
-    int index = (int) thread_index;
-    merge(value, index*n/2, index*n/2+n/4-1, index*n/2+n/2-1, sort);
-    return NULL;
-}
-
-void merge(int* value, int start, int mid, int end, int* test_sorted)
-{
-    int left_pointer = start;
-    int right_pointer = mid+1;
-
-    int total_array_size = end - start + 1;
-    
-    for(int merge_index = start; merge_index <= total_array_size+start; merge_index++){
-        if (left_pointer >= mid +1){
-            test_sorted[merge_index] = value[right_pointer];
-            right_pointer++;            
-        }
-        else if (right_pointer >= end + 1) {
-            test_sorted[merge_index] = value[left_pointer];
-            left_pointer++; 
-        }
-        else if (value[left_pointer] <= value[right_pointer]){
-            test_sorted[merge_index] =  value[left_pointer];
-            left_pointer ++;
-        }
-        else {
-            test_sorted[merge_index] = value[right_pointer];
-            right_pointer ++;
-        }
-
-    }
-
-    for(int merge_index = 0; merge_index <= total_array_size; merge_index++){
-        value[start+merge_index] = test_sorted[merge_index+start];
-    }
-}
-
-void merge_sort(int* value, int start_index, int end_index, int* sorted)
-{   
-    if (end_index <= start_index){
-        return;
-    }
-
-
-    int mid_index = (start_index+end_index)/2;
-
-    merge_sort(value, mid_index+1, end_index, sorted);
-    merge_sort(value, start_index, mid_index, sorted);
-
-
-    merge(value, start_index, mid_index, end_index, sorted);
-}
-
-void fillArray( int** values, unsigned int N )
-{
-    srand(time(0));
-	unsigned int i;
-    for (i=0; i<N; i++)
-        (*values)[i] = N-i;
-}
-
-void struct_merge_sort(int* value, int start_index, int end_index, int* sorted, int* point_index, int* sorted_int_index, bool* point_A,bool* sorted_int_A){
-    if (end_index <= start_index){
-        return;
-    }
-
-    int mid_index = (start_index+end_index)/2;
-
-    struct_merge_sort(value, mid_index+1, end_index, sorted, point_index, sorted_int_index, point_A, sorted_int_A);
-    struct_merge_sort(value, start_index, mid_index, sorted, point_index, sorted_int_index, point_A, sorted_int_A);
-
-    struct_merge(value, start_index, mid_index, end_index, sorted, point_index, sorted_int_index, point_A, sorted_int_A);
-}
-
-void struct_merge(int* value, int start, int mid, int end, int* test_sorted, int* point_index, int* sorted_int_index, bool* point_A,bool* sorted_int_A){
-    int left_pointer = start;
-    int right_pointer = mid+1;
-
-    int total_array_size = end - start + 1;
-    
-    for(int merge_index = start; merge_index <= total_array_size+start; merge_index++){
-        if (left_pointer >= mid +1){
-            test_sorted[merge_index] = value[right_pointer];
-            sorted_int_index[merge_index] = point_index[right_pointer];
-            sorted_int_A[merge_index] = point_A[right_pointer];
-            right_pointer++;            
-        }
-        else if (right_pointer >= end + 1) {
-            test_sorted[merge_index] = value[left_pointer];
-            sorted_int_index[merge_index] = point_index[left_pointer];
-            sorted_int_A[merge_index] = point_A[left_pointer];
-            left_pointer++;     
-        }
-        else if (value[left_pointer] < value[right_pointer]){
-            test_sorted[merge_index] = value[left_pointer];
-            sorted_int_index[merge_index] = point_index[left_pointer];
-            sorted_int_A[merge_index] = point_A[left_pointer];
-            left_pointer++;     
-        }
-        else {
-            test_sorted[merge_index] = value[right_pointer];
-            sorted_int_index[merge_index] = point_index[right_pointer];
-            sorted_int_A[merge_index] = point_A[right_pointer];
-            right_pointer++;     
-        }
-        printf("for value: %d index: %d , right_pointer: %d, left_pointer: %d\n",  test_sorted[merge_index], sorted_int_index[merge_index], right_pointer, left_pointer);
-    }
-
-    for(int merge_index = 0; merge_index <= total_array_size; merge_index++){
-        value[start+merge_index] = test_sorted[merge_index+start];
-        point_index[start+merge_index] = sorted_int_index[merge_index+start];
-        point_A[start+merge_index] = sorted_int_A[merge_index+start];
-    }
-
-}
-
-int main ( int argc, char* argv[] )
-{
-    unsigned int N = pow( 2, 5);
-    
-    int* values = (int*) malloc ( sizeof(int) * N );
-	fillArray ( &values, N );
-
-    printf("\n start \n \n");
-    for( int i = N - 50; i< N ; i++){
-        printf("%d \n", values[i]);
-    }
-    printf("\n");
-
-	int* sorted = (int*) malloc ( sizeof(int) * N );
-
-    mergeSortParallel(values, N, sorted);
-    printf("\n final sorted \n \n");
-        for( int i = 0; i< N ; i++){
-        printf("%d \n", sorted[i]);
-    }
-    printf("\n");
-	//Sorted array
 }
--- pth_msort.c
+++ pth_msort.c
@@ -2,12 +2,115 @@
 
 #include "pth_msort.h"
 
-void mergeSortParallel (const int* values, unsigned int N, int* sorted) {
+int* value;
+unsigned int n;
+int* sort;
+typedef struct 
+{
+    int index;
+    int left_last_index;
+    int right_last_index;
+    int right_size;
+    int left_size;
+} pth_msort;
 
-    for (unsigned int i = 0; i < N; i++){
-        sorted[i] = i;
+void merge_sort(int* value, int start_index, int end_index, int* sorted);
+void mergeSortParallel(const int* values, unsigned int N, int* sorted);
+void merge(int* value, int start, int mid, int end, int* test_sorted);
+void binary(const int* values, int value);
+void* merge_thread_handler(void* thread_index);
+void* sort_thread_handler(void* thread_index);
+void* para_merge_thread_handler(void* info);
+int compare_point(const int a , const int b);
+void printArray(int *arr, int size);
+void merge_tow_separated(int* value1, int start1, int size1, int start2, int size2, int* sorted, int index_sorted);
+int binary_search(const int* values, int start, int end, int value);
+
+void mergeSortParallel(const int* values, unsigned int N, int* sorted)
+{
+    value = (int*) values;
+    n = N;
+    sort = sorted;
+    int* value = (int*) values;
+    int start = 0;
+    int end = N-1;
+    pthread_t* handles = (pthread_t*) malloc (4*sizeof(pthread_t));
+    for (int thread_index =0; thread_index<4; thread_index++){
+        pthread_create(&handles[thread_index], NULL, sort_thread_handler, (void*)thread_index);  
+    } 
+
+    for (int thread_index =0; thread_index<4; thread_index++){
+        pthread_join(handles[thread_index], NULL);  
+    } 
+
+    // merge two merge sorts 
+
+    for (int thread_index =0; thread_index<2; thread_index++){
+        pthread_create(&handles[thread_index], NULL, merge_thread_handler, (void*)thread_index);  
+    } 
+
+    for (int thread_index =0; thread_index<2; thread_index++){
+        pthread_join(handles[thread_index], NULL);  
+    }  
+
+    int step = pow(2,n/8);
+    int section_left = step;
+    int section_right= N/2+1 + step;
+
+    int last_element_left=0;
+    int last_element_right=N/2;
+
+    for (int thread_index =0; thread_index<4; thread_index++){
+        int temp = binary_search(value, N/2, N-1, value[thread_index*n/8]);
+        pth_msort* thraed_index = (pth_msort*) malloc(sizeof(pth_msort));
+        thraed_index->index = thread_index;
+        thraed_index->left_last_index = thread_index*n/8;
+        thraed_index->left_size=thread_index*n/4;
+        thraed_index->right_last_index=0;
+        // merge_tow_separated(value, thread_index*n/8, n/8, n/2 + thread_index*n/8, n/8, sort, thread_index*n/4);
+        pthread_create(&handles[thread_index], NULL, para_merge_thread_handler, (void*)thraed_index);  
+    } 
+    for (int thread_index =0; thread_index<4; thread_index++){
+        pthread_join(handles[thread_index], NULL);  
+    } 
+
+    merge_tow_separated(value,0,N/2,0,N/2,sort,0);
+}
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d \n", arr[i]);
     }
+    printf("\n");
     
+}
+
+int compare_int(const int a , const int b) {
+    if (a <= b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    }
+}
+
+void* sort_thread_handler(void* thread_index){
+    int index = (int) thread_index;
+    merge_sort(value, index*n/4, (index+1)*n/4-1, sort);
+    return NULL;
+}
+
+void* merge_thread_handler(void* thread_index){
+    int index = (int) thread_index;
+    merge(value, index*n/2, index*n/2+n/4-1, index*n/2+n/2-1, sort);
+    return NULL;
+}
+
+void* para_merge_thread_handler(void* thread_index){
+    pth_msort* index = (pth_msort*) thread_index;
+    merge_tow_separated(value, index->index*n/8, n/8, n/2 + index->index*n/8, n/8, sort, index->index*n/4);
+    return NULL;
 }
 
 void merge(int* value, int start, int mid, int end, int* test_sorted)
@@ -17,7 +120,7 @@ void merge(int* value, int start, int mid, int end, int* test_sorted)
 
     int total_array_size = end - start + 1;
     
-    for(int merge_index = 0; merge_index <= total_array_size; merge_index++){
+    for(int merge_index = start; merge_index <= total_array_size+start; merge_index++){
         if (left_pointer >= mid +1){
             test_sorted[merge_index] = value[right_pointer];
             right_pointer++;            
@@ -37,9 +140,8 @@ void merge(int* value, int start, int mid, int end, int* test_sorted)
 
     }
 
-    
     for(int merge_index = 0; merge_index <= total_array_size; merge_index++){
-        value[start+merge_index] = test_sorted[merge_index];
+        value[start+merge_index] = test_sorted[merge_index+start];
     }
 }
 
@@ -57,4 +159,62 @@ void merge_sort(int* value, int start_index, int end_index, int* sorted)
 
 
     merge(value, start_index, mid_index, end_index, sorted);
+}
+
+int binary_search(const int* values, int start, int end, int value)
+{
+    if ( end - start < 2 &&  values[end] != value && values[start] != value){
+        return end;
+    }
+    if (value < values[start])
+    {
+        return start;
+    }
+
+    if (value == values[start] || value < values[start]){
+        return start;
+    }
+    else if(value == values[end] || value > values[end]){
+        return end;
+    }
+    else if(value == values[(start+end)/2]){
+        return (start+end)/2;
+    }
+    else if (value < values[(start+end)/2]){
+        return binary_search(values, start, (start+end)/2, value);
+    }
+    else{
+        return binary_search(values, (start+end)/2+1, end, value);
+    }
+}
+
+void merge_tow_separated(int* value, int start1, int size1, int start2, int size2, int* sorted, int index_sorted)
+{
+    int left_pointer = start1;
+    int right_pointer = start2;
+    int total_array_size = size1+size2;
+    merge(value, left_pointer, right_pointer, total_array_size, sorted);
+    
+    for(int merge_index = index_sorted; merge_index < total_array_size+index_sorted; merge_index++){
+        if (left_pointer >= start1+size1){
+            sorted[merge_index] = merge_index;
+            right_pointer++;            
+        }
+        else if (right_pointer >= start2 + size2) {
+            sorted[merge_index] = merge_index;
+            left_pointer++; 
+        }
+        else if (value[left_pointer] <= value[right_pointer]){
+            sorted[merge_index] =  merge_index;
+            left_pointer ++;
+        }
+        else {
+            sorted[merge_index] = merge_index;
+            right_pointer ++;
+        }
+
+    }
+    for(int merge_index = index_sorted; merge_index < total_array_size+index_sorted; merge_index++){
+        value[merge_index]=sorted[merge_index];
+    }
 }
